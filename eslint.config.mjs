a3ab5c9be50fdfd994882c95d9228a7e8c560641
Francisco J Franco  Jaramillo <83324177+ffranco10@users.noreply.github.com>
2025-12-03 12:46:30 -0800

    Enhance ESLint config with TypeScript support
    Added TypeScript parser and plugin to ESLint configuration.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,9 +11,19 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+/**
+ * mixed configuration:
+ * - Next.js
+ * - TypeScript (parser + plugin)
+ * - Personalized rules (include NO EXPLICIT ANY OFF)
+ */
+export default [
+  // Configs base de Next.js
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Override with my rules
   {
+    files: ["**/*.{ts,tsx}"],
     ignores: [
       "node_modules/**",
       ".next/**",
@@ -19,10 +31,21 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: "./tsconfig.json",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "react-hooks/exhaustive-deps": "warn"
     },
   },
 ];
-
-export default eslintConfig;
