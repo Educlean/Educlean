dc6c73a7274215f4f2ba969bafaa7a9e331357f7
Andres Castaneda <126159640+yo-ac@users.noreply.github.com>
2025-12-03 17:51:52 -0500

    Add layout support to MyApp component
    Enhance MyApp component to support custom layouts.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,39 @@
 // pages/_app.tsx
 import '../styles/globals.css';
 
+import React from 'react';
 import type { AppProps } from 'next/app';
 import Layout from './layout';
 import { UserProvider } from '../context/UserContext';
 
+// Support pages setting `noLayout = true` or providing a `getLayout` function.
+type ComponentWithLayout = AppProps['Component'] & {
+  noLayout?: boolean;
+  getLayout?: (page: React.ReactNode) => React.ReactNode;
+};
+
 export default function MyApp({ Component, pageProps }: AppProps) {
-  console.log('Component name:', Component.name);
-  if (Component.name === "Page") {
+  const Comp = Component as ComponentWithLayout;
+
+  // If the page provides a getLayout function, use it (recommended pattern).
+  if (typeof Comp.getLayout === 'function') {
+    return (
+      <UserProvider>
+        {Comp.getLayout(<Component {...pageProps} />)}
+      </UserProvider>
+    );
+  }
+
+  // If the page opts out of the layout via a static property `noLayout`, render plainly.
+  if (Comp.noLayout) {
     return (
       <UserProvider>
         <Component {...pageProps} />
       </UserProvider>
     );
   }
-  
+
+  // Default: wrap with Layout.
   return (
     <UserProvider>
       <Layout>
