7c01a0c6b2e66d8429d8e288e7f74eae2d9f0a98
Andres Castaneda <126159640+yo-ac@users.noreply.github.com>
2025-12-06 21:38:09 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+
+  // 🔥 IMPORTANTE PARA QUE VERCEL IGNORE ESLINT
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
+
+export default nextConfig;
+
 
 export default nextConfig;
