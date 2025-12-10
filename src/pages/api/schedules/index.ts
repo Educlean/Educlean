0cea33606a76728b57cd20ba9c9adb2a45f4a53a
Andres Castaneda <andres.castaeda97@gmail.com>
2025-12-10 13:35:48 -0500

    UPLOAD FILED LOGIC UPDATED TO PROCESS SATURDAY AND SUNDAYS

--- src/pages/api/schedules/index.ts
+++ src/pages/api/schedules/index.ts
@@ -21,11 +21,13 @@ interface ScheduleRow {
   Wed?: string;
   Thu?: string;
   Fri?: string;
+  Sat?: string;
+  Sun?: string;
   schoolID: string;
 }
 
-type Day = "Mon" | "Tue" | "Wed" | "Thu" | "Fri";
-const days: Day[] = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+type Day = "Mon" | "Tue" | "Wed" | "Thu" | "Fri" | "Sat" | "Sun";
+const days: Day[] = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
 export default async function handler(
   req: NextApiRequest,
