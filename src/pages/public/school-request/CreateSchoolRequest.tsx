e7537d5bb84477c49d14950c35593cd398d910fc
Andres Castaneda <andres.castaeda97@gmail.com>
2026-01-12 21:14:49 -0500

    Form for schools to create requested implemented

--- src/pages/public/school-request/CreateSchoolRequest.tsx
+++ src/pages/public/school-request/CreateSchoolRequest.tsx
@@ -143,7 +143,7 @@ export default function CreateSchoolRequest() {
             <div className="flex justify-end gap-3">
               <button
                 type="submit"
-                className="px-5 py-2 rounded-md bg-green-500 text-white"
+                className="px-5 py-2 rounded-md bg-green-500 text-white cursor-pointer hover:bg-green-600 transition"
               >
                 Submit request
               </button>
